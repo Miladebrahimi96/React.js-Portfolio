946e617ecb18ac33b9b48d2fe0005d6815dabf8b
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-11 22:31:20 +0430

    Hero component styled

--- src/components/shared/Hero.js
+++ src/components/shared/Hero.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+//styles
+import styles from "./Hero.module.css";
 
 //images
 import heroImage from "../../images/hero.jpg";
 
 const Hero = () => {
     return (
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.titleContainer}>
                 <h1>Milad Ebrahimi</h1>
                 <h2>Junior Front-End Developer</h2>
+                <Link to="">Go to Projects</Link>
             </div>
-            <img src={heroImage} alt="Hero Image"/>
+            <img src={heroImage} alt="Hero"/>
+
         </div>
     );
 };

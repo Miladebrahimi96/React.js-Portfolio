1daec9402e3216854d9a5162d139c0f40735c073
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-12 16:40:54 +0430

    Projects styled responsive design

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,33 +1,38 @@
 import React from 'react';
 
+//Styles
+import styles from "./Projects.module.css";
+
 //Components
 import Project from './shared/Project';
 
 const Projects = () => {
     return (
-        <div>
-            <h2>Explore Projetcs Here</h2>
-            <Project
-                github="https://github.com/Miladebrahimi96/Weather-App-React-ReactRoutes"
-                demo="https://github.com/Miladebrahimi96/Weather-App-React-ReactRoutes"
-                title="Weather Forecast"
-            />
-            <Project
-                github="https://github.com/Miladebrahimi96/Digital-Currency-Exchange-Reactjs"
-                demo="https://github.com/Miladebrahimi96/Digital-Currency-Exchange-Reactjs"
-                title="Digital Currency"
-            />
-            <Project
-                github="https://github.com/Miladebrahimi96/React-form-SPA"
-                demo="https://github.com/Miladebrahimi96/React-form-SPA"
-                title="Sign UP/Login Form"
-            />
-            <Project
-                github="https://github.com/Miladebrahimi96/Calculator"
-                demo="https://github.com/Miladebrahimi96/Calculator"
-                title="Simple Calculator"
-            />
-        </div>
+        <>
+            <h2 className={styles.title}>Explore Projetcs Here</h2>
+            <div className={styles.container}>
+                <Project
+                    github="https://github.com/Miladebrahimi96/Weather-App-React-ReactRoutes"
+                    demo="https://github.com/Miladebrahimi96/Weather-App-React-ReactRoutes"
+                    title="Weather Forecast"
+                />
+                <Project
+                    github="https://github.com/Miladebrahimi96/Digital-Currency-Exchange-Reactjs"
+                    demo="https://github.com/Miladebrahimi96/Digital-Currency-Exchange-Reactjs"
+                    title="Digital Currency"
+                />
+                <Project
+                    github="https://github.com/Miladebrahimi96/React-form-SPA"
+                    demo="https://github.com/Miladebrahimi96/React-form-SPA"
+                    title="Sign UP/Login Form"
+                />
+                <Project
+                    github="https://github.com/Miladebrahimi96/Calculator"
+                    demo="https://github.com/Miladebrahimi96/Calculator"
+                    title="Simple Calculator"
+                />
+            </div>
+        </>
     );
 };
 

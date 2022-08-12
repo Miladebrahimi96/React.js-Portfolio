a46b3d5b7764f1a56ac9d82f5582169a1070fe86
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-12 16:01:15 +0430

    Another div created in Project to hold img & p

--- src/components/shared/Project.js
+++ src/components/shared/Project.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
+//Styles
+import styles from "./Project.module.css";
+
 //folder icon
 import folderIcon from "../../images/folder.png";
 
 const Project = ({github, demo, title}) => {
     return (
-        <div>
-            <img src={folderIcon} alt="icon"/>
-            <p>{title}</p>
+        <div className={styles.container}>
+            <div className={styles.folderContainer}>
+                <img src={folderIcon} alt="icon"/>
+                <p>{title}</p>
+            </div>
             <a href={github}>GitHub</a>
             <a href={demo}>Live Demo</a>
         </div>

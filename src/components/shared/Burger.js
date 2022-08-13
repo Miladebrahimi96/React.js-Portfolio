43cb3817356125e82d5c8c1c9c17f9c1664c7bd0
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-13 15:16:18 +0430

    Burger component initialized

---
+++ src/components/shared/Burger.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Navbar from '../Navbar';
+
+class Burger extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <Navbar />
+            </>
+        );
+    }
+}
+
+export default Burger;
